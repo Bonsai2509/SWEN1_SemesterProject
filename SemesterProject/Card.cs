ca1ee70a4e2b5e59b925fe760ce184fbd0d91c7c
Bonsai2509 <if22b071@technikum-wien.at>
2023-09-21 15:52:40 +0200

    Add project files.

---
+++ SemesterProject/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterProject
+{
+    internal class Card
+    {
+        public Card(string type, string name, int damage, string damagetype)
+        {
+            CardType = type;
+            Name = name;
+            Damage = damage;
+            DamageType = damagetype;
+        }
+        public string CardType { get; private set; }
+        public string Name { get; private set; }
+        public int Damage { get; set; } = 0;
+        public string DamageType { get; set; };
+    }
+}
---
+++ SemesterProject/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterProject
+{
+    internal class Player
+    {
+        public Player(string name, string pw, int money=20)
+        {
+            Username = name;
+            Password = pw;
+            coins = money;
+        }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public int coins { get; set; } = 0;
+        public List<Card>? Deck;
+        public List<Card>? Stack;
+
+    }
+}
